4febc0d8223090ab3c59e79dc30234a794254bb9
kien <kien@tentou.tech>
2025-06-30 09:31:29 +0000

    remove unsupported chains

--- lib/util/chain.ts
+++ lib/util/chain.ts
@@ -1,12 +1,4 @@
 export enum ChainId {
-  MAINNET = 1,
-  UNICHAIN = 130,
-  BASE = 8453,
-  OPTIMISM = 10,
-  ARBITRUM_ONE = 42161,
-  POLYGON = 137,
-  SEPOLIA = 11155111,
-  GÖRLI = 5,
   STORY_AENEID = 1315,
   STORY = 1514
 }
@@ -14,13 +6,6 @@ export enum ChainId {
 // If you update SUPPORTED_CHAINS, ensure you add a corresponding RPC_${chainId} environment variable.
 // lib/config.py will require it to be defined.
 export const SUPPORTED_CHAINS = [
-  ChainId.MAINNET,
-  ChainId.POLYGON,
-  ChainId.SEPOLIA,
-  ChainId.GÖRLI,
-  ChainId.ARBITRUM_ONE,
-  ChainId.BASE,
-  ChainId.UNICHAIN,
   ChainId.STORY_AENEID,
   ChainId.STORY,
 ]
